68fd7021e85c692778e768b4886fa6abe48b3d63
JSotto09 <jacsotto09@gmail.com>
2022-02-22 16:46:38 -0500

    added new servlets

---
+++ SkittwhaleMarriageInc/src/main/java/com/revature/servlets/Florists.java
@@ -0,0 +1,20 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/florist")
+public class Florists extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("<h1>Florists</h1>");
+		resp.getWriter().write("<h2>Want to pick out flowers for your pokemons' wedding?</h2>");
+		resp.getWriter().write("<p>Here is a list of our florists:</p>");
+	}
+	
+}
